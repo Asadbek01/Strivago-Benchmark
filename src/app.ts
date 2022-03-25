9c9fd5385b28170b5780e9286a7f3403421e0028
yonatanski <phpcodeyg@gmail.com>
2022-03-25 16:48:32 +0100

    cors enabled

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,16 @@ import express from "express";
 
 import accommodationRouter from "./accommodation";
 import usersRouter from "./users"
+import cors from "cors"
 
 const server = express();
 
 process.env.TS_NODE_DEV && require("dotenv").config();
 
 server.use(express.json());
+server.use(cors())
 
-// cors middleware's
+// // cors middleware's
 // server.use(cors);
 
 // Endpoints
